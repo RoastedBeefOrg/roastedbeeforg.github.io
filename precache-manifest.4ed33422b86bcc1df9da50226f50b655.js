437757b4867edd985eba5af7dea65328a0879548
DeFiDev <defidev2021@gmail.com>
2022-05-05 00:16:15 -0700

    update

---
+++ precache-manifest.4ed33422b86bcc1df9da50226f50b655.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "eef607ecffc1e4f74030",
+    "url": "/static/css/main.eeadacfb.chunk.css"
+  },
+  {
+    "revision": "eef607ecffc1e4f74030",
+    "url": "/static/js/main.eef607ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1eede07ee500e8bd92b2",
+    "url": "/static/css/2.328f7730.chunk.css"
+  },
+  {
+    "revision": "1eede07ee500e8bd92b2",
+    "url": "/static/js/2.1eede07e.chunk.js"
+  },
+  {
+    "revision": "017e36915e37cadea492a6495e9c9c45",
+    "url": "/static/media/logo.017e3691.png"
+  },
+  {
+    "revision": "d2175b68cb191e4088f84aa5d50ea75f",
+    "url": "/static/media/fb.d2175b68.png"
+  },
+  {
+    "revision": "b28d5f0e91de954e0481aa4b6b5d2810",
+    "url": "/static/media/airdrop.b28d5f0e.png"
+  },
+  {
+    "revision": "7c53da97a85fe396bfa920daa1a50022",
+    "url": "/static/media/butonBg.7c53da97.gif"
+  },
+  {
+    "revision": "bdfe218512f3807baeb1510085b2f76c",
+    "url": "/index.html"
+  }
+];
